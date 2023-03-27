18e92ae52d2d89939980d276a97b62d6903bf054
Sinan Ozdemir <sinan.u.ozdemir@gmail.com>
2023-03-27 07:19:31 -0700

    flask app

--- README.md
+++ README.md
@@ -20,6 +20,51 @@ We will use tools including large pre-trained models and model training dashboar
 
 [Production Optimization](notebooks/4_deployment_and_optimization.ipynb)
 
+### Flask App
+
+`app.py` is a Flask app that uses a VGG16 model to classify the art style of an uploaded image. The app currently supports 10 different art styles:
+
+Abstract Expressionism
+Art Nouveau (Modern)
+Baroque
+Expressionism
+Impressionism
+Northern Renaissance
+Post-Impressionism
+Realism
+Romanticism
+Symbolism
+
+Start the Flask app:
+`python app.py`
+
+This should start the Flask app and make it available at `http://localhost:5000`.
+
+#### How to Use the App
+
+To classify an image, you can use a cURL request in the following format:
+
+
+```curl -X POST -F 'image=@/path/to/your/image.jpg' http://localhost:5000/predict```
+
+Replace `/path/to/your/image.jpg` with the path to your own image. The response will be in JSON format and will contain the predicted art style and its confidence score, as shown below:
+
+```
+{
+	"class_id": "Impressionism",
+	"confidence": 0.9759522089958191
+}
+```
+
+If there is an error with the request, such as no image being provided, the response will contain an error message instead:
+
+```
+{
+	"error": "No image provided"
+}
+```
+
+
 ## Instructor
 
 **Sinan Ozdemir** is the Founder and CTO of LoopGenius where he uses State of the art AI to help people create and run their businesses. Sinan is a former lecturer of Data Science at Johns Hopkins University and the author of multiple textbooks on data science and machine learning. Additionally, he is the founder of the recently acquired Kylie.ai, an enterprise-grade conversational AI platform with RPA capabilities. He holds a master’s degree in Pure Mathematics from Johns Hopkins University and is based in San Francisco, CA.
---
+++ app.py
@@ -0,0 +1,66 @@
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+from PIL import Image
+from flask import Flask, request, jsonify
+from torch import nn
+
+# Initialize the Flask app
+app = Flask(__name__)
+
+# Define the image transformations to be applied
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),  # Resize the input image to 224 x 224 pixels
+    transforms.ToTensor(),  # Convert the image to a PyTorch tensor
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],  # Normalize the image using the VGG16 mean and standard deviation values
+        std=[0.229, 0.224, 0.225])
+])
+
+# Load the pre-trained VGG16 model
+trained_vgg_model = models.vgg16()
+
+# Modify the model's final layer to have the correct number of output classes
+num_classes = 10
+trained_vgg_model.classifier[-1] = nn.Linear(trained_vgg_model.classifier[-1].in_features, num_classes)
+
+# Set the model to evaluation mode and load the trained weights
+trained_vgg_model.eval()
+trained_vgg_model.load_state_dict(torch.load("data/trained_vgg_model_pruned.pt"))
+
+# Define the class labels
+CLASSES = [
+    'Abstract_Expressionism',
+    'Art_Nouveau_Modern',
+    'Baroque',
+    'Expressionism',
+    'Impressionism',
+    'Northern_Renaissance',
+    'Post_Impressionism',
+    'Realism',
+    'Romanticism',
+    'Symbolism'
+]
+
+# Define the predict endpoint
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.files.get('image'):  # Check if an image file was uploaded
+        # Preprocess the image and pass it through the model
+        image = Image.open(request.files['image'].stream).convert('RGB')
+        input_tensor = transform(image).unsqueeze(0)
+        with torch.no_grad():
+            output = trained_vgg_model(input_tensor)
+
+        # Get the predicted class and its confidence score
+        _, predicted = torch.max(output, 1)
+        class_id = int(predicted.item())
+        confidence = torch.softmax(output, dim=1)[0][class_id].item()
+
+        # Return the predicted class and its confidence score as a JSON response
+        return jsonify({'class_id': CLASSES[class_id], 'confidence': confidence})
+
+    return jsonify({'error': 'No image provided'})  # Return an error message if no image file was uploaded
+
+if __name__ == '__main__':
+    app.run()  # Run the Flask app if this module is executed directly (not imported)
